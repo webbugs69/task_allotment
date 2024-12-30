1a7ffd67a9f4ac0ab1bb72ed728efd98e8f24f55
webbugs69 <webbugs661@gmail.com>
2024-12-30 16:31:07 +0530

    Update models.py

--- task_allotment/task_app/models.py
+++ task_allotment/task_app/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+
+class details(models.Model):
+    username = models.CharField(max_length=100 , unique= True)
+    email = models.EmailField(max_length= 254 , default= 'null ' ,unique= True )
+    password = models.CharField(max_length= 100)
+    role = models.CharField(max_length=100)
+    department = models.CharField(max_length= 100 , default = 'default' , blank = True)
+    
+    def __str__(self):
+        return self.username
